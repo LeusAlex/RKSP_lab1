d4348c62ca067a6c328fa72daa45d07b816c9663
LeusAlex <sanya.leus@yandex.ru>
2025-12-16 03:27:47 +0300

    Шаг 1.

---
+++ src/main/java/ru/rksp/Leus/LeusApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.Leus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rksp/Leus/LeusApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.Leus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
